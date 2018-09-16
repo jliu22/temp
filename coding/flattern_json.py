b9c45d06e95e623a8bb93b520c2a7b5c9c657dab
jliu <jliu@riverbed.com>
2018-09-15 21:12:16 -0700

    update code

--- coding/flattern_json.py
+++ coding/flattern_json.py
@@ -18,7 +18,22 @@ to
 
 import collections
 
+
 def flatten(d, parent_key='', sep='_'):
+
+    items = []
+
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+
+    return dict(items)
+
+
+def flatten2(d, parent_key='', sep='_'):
 
     items = []
 
@@ -38,7 +53,11 @@ def flatten(d, parent_key='', sep='_'):
     return collections.OrderedDict(items)
 
 if __name__== "__main__":
+    d = {'a': 1, 'c': {'a': 2, 'b': {'x': 5, 'y' : 10}}, 'd': [1, 2, 3]}
+    print d
+    print flatten(d)
+
     d = {'a': 1, 'c': {'a': 2, 'b': {'x': 5, 'y' : 10}}, 'd': [{'e' : [1, 2, 3]}, {'f' : 2}, {'g' : 3}]}
     print d
     print flatten(d)
-    print flatten(d).items()
+    print dict(flatten2(d).items())
